8cb2c23ba5bace28aa32765925edbf13e141b0ce
rafaellaevboff <rafaella-eboff@rede.ulbra.br>
2022-12-28 15:29:22 -0300

    Cria tela inicial com modal e opções de adicionar ao carrinho aas compras

---
+++ app.js
@@ -0,0 +1,80 @@
+const vm = new Vue({
+
+    el:"#app",
+    data:{
+        produtos: [],
+        produto: false,
+        carrinho: [],
+        
+    },
+    filters:{
+        numeroPreco(valor) {
+            return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL"})
+        },
+        maiusculo(valor) {
+            return valor.toUpperCase();
+        }
+    },
+    computed: {
+        carrinhoTotal(){
+            let total = 0;
+            if(this.carrinho.length){
+                this.carrinho.forEach(item =>{
+                    total += item.preco
+                    console.log(item);
+                }) //ele recalcula sempre que o tamanho do carrinho for alterado, por isso o total de R$ no carrinho atualiza ao add e ao excluir
+            }
+            return total;
+        }
+    },
+    methods:{
+        fetchProdutos(){
+            fetch("./api/produtos.json")
+            .then(r => r.json())
+            .then(r => {
+                this.produtos = r;
+            })
+        },
+        fetchProduto(id) {
+            fetch(`./api/produtos/${id}/dados.json`)
+            .then(r => r.json())
+            .then(json => {
+                this.produto = json
+            })
+        },
+        abrirModal(id){
+            this.fetchProduto(id)
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            })
+        },
+        fecharModal({target, currentTarget}){
+            // console.log(event.target); //é exatamente onde se é clicado
+            // console.log(event.currentTarget); //é onde está o evento atribuido
+            if(target === currentTarget) this.produto = false
+        },
+        adicionarItem(){
+            this.produto.estoque--
+            const {id, nome, preco} = this.produto
+            // console.log(id, nome, preco);
+            this.carrinho.push({id, nome, preco})
+        },
+        removerItem(index){
+            this.carrinho.splice(index,1) //o index é onde o elemento q quer excluir está na lista do carrinho
+        },
+        checarLocalStorage(){
+            if(window.localStorage.carrinho)
+                this.carrinho = JSON.parse(window.localStorage.carrinho);
+        }
+    },
+    watch:{
+        carrinho(){
+            window.localStorage.carrinho = JSON.stringify(this.carrinho)
+        }
+    },
+    created(){
+        this.fetchProdutos()
+        this.checarLocalStorage()
+    }
+})
